e70c542bab22e2e07423ac5625c2efa76245ee48
Александр <prudnikov21@icloud.com>
2021-11-06 17:06:17 +0200

    vg

--- module_3/myvenv/ads/views.py
+++ module_3/myvenv/ads/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+import random
 
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+calls_counter = [0]
+
+
+class HomeAds(TemplateView):
+    template_name = 'ads_home.html'
+
+    def get_context_data(self, **kwargs):
+        ads_list = ['элемент списка', 'картошка', 'морква', 'порква', 'горква']
+        random_list = []
+        for x in range(0, 5):
+            random_list.append(random.choice(ads_list))
+        context = super().get_context_data(**kwargs)
+        context['ads_list'] = random_list
+        context['calls'] = calls_counter[0]
+        calls_counter[0] += 1
+        return context
+
+    def post(self, request, *args, **kwargs):
+        print('yes done')
+        return 0
